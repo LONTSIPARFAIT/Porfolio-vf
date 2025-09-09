43153024bc8761c35cfcea32e783ac328599a5f5
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-09 23:41:19 +0100

    first commit

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,4 +1,4 @@
-import Title from './Title'
+import Title from './Title' 
 import imgCSS from "../assets/techno/css.png";
 import imgJS from "../assets/techno/js.png";
 import imgREACT from "../assets/techno/react.png";
@@ -27,8 +27,28 @@ const Experience = () => {
     <div>
       <Title title="Expériences" />
       <div className="" >
+        <div className="" >
+          {skills.map((skill) => (
+            <div key={skill.id} className="flex flex-col md:flex-row items-center bg-base-100 p-5 rounded-xl md:w-96 shadow-xl">
+
+              <div className="md:ml-4 text-center md:text-left">
+                <img src={skill.image} 
+                  alt="Lontsi Parfait" 
+                  className="w-96 h-96 object-cover border-8 border-accent shadow-xl" 
+                  style={
+                    {
+                      borderRadius :  "30% 70% 70% 30% / 67% 62% 38% 33%"
+                }}/>
+              </div>
+              <span className="text-sm mt-2">{skill.name}</span>
+              <p className="text-sm"> {skill.description} </p>
+
+            </div>
+          ))}
+        </div>
+
         <div className="" ></div>
-        <div className="" ></div>
+
       </div>
     </div>
   )
