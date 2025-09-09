4b3018a93429b31c97fec0585f3f5ea6710b3a56
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-09 23:22:42 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import './App.css'
-import About from './components/About'
-import Home from './components/Home'
-import NavBar from './components/Navbar'
+import "./App.css";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Home from "./components/Home";
+import NavBar from "./components/Navbar";
 
 export default function App() {
-
   return (
     <div>
       <div className="p-5 md:px-[15%] ">
         <NavBar />
-      </div>
         <Home />
+      </div>
       <About />
+      <div className="p-5 md:px-[15%] ">
+        <Experience />
+      </div>
     </div>
-  )
+  );
 }
