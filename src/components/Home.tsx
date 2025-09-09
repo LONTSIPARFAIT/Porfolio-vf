119c2a2957fdf2d573408d58f9efa41bf9e295c7
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-09 18:13:04 +0100

    first commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,6 +5,10 @@ const Home = () => {
         <h1 className="text-5xl md:text-6xl font-bold text-center md:text-left mt-4 md:mt-0">Bonjour , <br /> Je suis {" "}
           <span className="text-accent">PerfectoDev</span> 
         </h1>
+        <p className="text-lg text-center md:text-left mt-4 text-gray-600">
+          Développeur web et mobile passionné par la création d'applications innovantes et performantes.
+        </p>
+    
       </div>
 
       <div className="d">x</div>
