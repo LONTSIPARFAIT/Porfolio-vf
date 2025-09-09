2eca8aae3ca7bededa31fc03ec58704fb91e9d27
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-09 17:47:45 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,11 @@ import NavBar from './components/NavBar'
 export default function App() {
 
   return (
-    <>
-      <NavBar />
-    </>
+    <div>
+      <div className="p-5 lg:px-[15%] ">
+        <NavBar />
+      </div>
+      
+    </div>
   )
 }
