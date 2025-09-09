e3ac647fdae9cc9733012c730e753d99672a11a8
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-09 22:42:34 +0100

    first commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,17 +7,17 @@ const About = () => {
       <Title title="A Propos" />
 
       <div className="md:h-screen flex justify-center">
+
         <div className="">
           <img
             src={img}
             alt="Lontsi Parfait"
-            className="w-96 h-96 object-cover border-8 border-accent shadow-xl"
-            style={{
-              borderRadius: "30% 70% 70% 30% / 67% 62% 38% 33%",
-            }}
+            className="w-96 object-cover rounded-xl "
           />
         </div>
+
         <div className=""></div>
+        
       </div>
     </div>
   );
