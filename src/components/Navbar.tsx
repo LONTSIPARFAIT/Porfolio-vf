e139d50e563c82274847d56ec2dd2faaef7ae25e
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-09 17:41:00 +0100

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Container } from "luciDe-react";
 
 const NavBar = () => {
   return (
-    <div className="flex items-center font-bold text-3xl md:text-xl">
+    <div className="flex justify-center md:justify-between items-center p-4 text-3xl ">
       <a href="#" className="flex items-center font-bold text-3xl md:text-xl">
         <Container className="mr-2"/>
         Perfecto
