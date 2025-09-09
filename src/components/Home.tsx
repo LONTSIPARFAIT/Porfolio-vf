97a45e30bd77a8836e4044ee958565b8d128f683
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-09 21:49:35 +0100

    first commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,10 @@
 import { Mail } from "luciDe-react";
+import img from "../assets/img2.jpg";
 
 const Home = () => {  
   return (
     <div className="flex flex-col-reverse md:flex-row items-center justify-center md:my-32 my-10">
+
       <div className="flex flex-col">
         <h1 className="text-5xl md:text-6xl font-bold text-center md:text-left mt-4 md:mt-0">Bonjour , <br /> Je suis {" "}
           <span className="text-accent">PerfectoDev</span> 
@@ -18,7 +20,9 @@ const Home = () => {
     
       </div>
 
-      <div className="d">x</div>
+      <div className="d">
+        <img src="" alt="Lontsi Parfait" />
+      </div>
 
     </div>
   );
