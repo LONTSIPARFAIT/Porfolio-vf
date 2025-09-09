56ee14e4fd43b0bb43d56281ea8c8009005a2713
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-09 23:28:57 +0100

    first commit

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import Title from './Title'
+import imgCSS from "../assets/techno/css.png";
+import imgJS from "../assets/techno/js.png";
+import imgREACT from "../assets/techno/react.png";
+import imgHTML from "../assets/techno/html.png";
+import imgNEXT from "../assets/techno/next-js.webp";
+import imgNODE from "../assets/techno/node-js.png";
+import imgTYPE from "../assets/techno/typescript.svg";
+import imgTAILWIND from "../assets/techno/tailwind.png";
+import imgPRISMA from "../assets/techno/prisma.webp";
+
+
+const skills = [
+    { id: 1, name: "HTML", image: imgHTML },
+    { id: 2, name: "CSS", image: imgCSS },
+    { id: 3, name: "JavaScript", image: imgJS },
+    { id: 4, name: "React", image: imgREACT },
+    { id: 5, name: "Node.js", image: imgNODE },
+    { id: 6, name: "Tailwind CSS", image: imgTAILWIND },
+    { id: 7, name: "TypeScript", image: imgTYPE },
+    { id: 8, name: "Next.js", image: imgNEXT },
+    { id: 9, name: "Prisma", image: imgPRISMA },
+];
 
 const Experience = () => {
   return (
     <div>
-      red
+      <Title title="Expériences" />
+      .
     </div>
   )
 }
