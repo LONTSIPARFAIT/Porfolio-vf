b61da60d8612cfde6177bea1441509c66f2b64a6
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-09 22:37:42 +0100

    first commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,9 +5,14 @@ const About = () => {
   return (
     <div className="bg-base-300 p-10 mb-10 md:mb-32">
       <Title title="A Propos"/>
-      <div className="md:h-screen flex justify-center">
 
+      <div className="md:h-screen flex justify-center">
+        <div className="">
+            
+        </div>
+        <div className=""></div>
       </div>
+
     </div>
   )
 }
