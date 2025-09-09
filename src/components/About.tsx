9ce2f3f15f6c25ed0454e74799e116f240423f88
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-09 22:39:00 +0100

    first commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,20 +1,26 @@
-import Title from "./Title"
-
+import Title from "./Title";
+import img from "../assets/img3.jpg";
 
 const About = () => {
   return (
     <div className="bg-base-300 p-10 mb-10 md:mb-32">
-      <Title title="A Propos"/>
+      <Title title="A Propos" />
 
       <div className="md:h-screen flex justify-center">
         <div className="">
-            
+          <img
+            src={img}
+            alt="Lontsi Parfait"
+            className="w-96 h-96 object-cover border-8 border-accent shadow-xl"
+            style={{
+              borderRadius: "30% 70% 70% 30% / 67% 62% 38% 33%",
+            }}
+          />
         </div>
         <div className=""></div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
