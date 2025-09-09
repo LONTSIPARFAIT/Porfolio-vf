94f7f18eba627ef9de5228e51b1e62bf2f023ffe
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-10 00:13:03 +0100

    first commit

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,4 +1,4 @@
-import Title from './Title' 
+import Title from "./Title";
 import imgCSS from "../assets/techno/css.png";
 import imgJS from "../assets/techno/js.png";
 import imgREACT from "../assets/techno/react.png";
@@ -12,96 +12,108 @@ import google from "../assets/companies/google.png";
 import meta from "../assets/companies/meta.webp";
 import amazon from "../assets/companies/amazon.png";
 
-
 const skills = [
-    { id: 1, name: "HTML", image: imgHTML },
-    { id: 2, name: "CSS", image: imgCSS },
-    { id: 3, name: "JavaScript", image: imgJS },
-    { id: 4, name: "React", image: imgREACT },
-    { id: 5, name: "Node.js", image: imgNODE },
-    { id: 6, name: "Tailwind CSS", image: imgTAILWIND },
-    { id: 7, name: "TypeScript", image: imgTYPE },
-    { id: 8, name: "Next.js", image: imgNEXT },
-    { id: 9, name: "Prisma", image: imgPRISMA },
+  { id: 1, name: "HTML", image: imgHTML },
+  { id: 2, name: "CSS", image: imgCSS },
+  { id: 3, name: "JavaScript", image: imgJS },
+  { id: 4, name: "React", image: imgREACT },
+  { id: 5, name: "Node.js", image: imgNODE },
+  { id: 6, name: "Tailwind CSS", image: imgTAILWIND },
+  { id: 7, name: "TypeScript", image: imgTYPE },
+  { id: 8, name: "Next.js", image: imgNEXT },
+  { id: 9, name: "Prisma", image: imgPRISMA },
 ];
 
 const experiences = [
-    {
-        id: 1,
-        role: "Software Engineer",
-        company: "Google",
-        period: "Sep 2022 - Présent",
-        description: [
-            "Développement de nouvelles fonctionnalités pour Google Maps.",
-            "Optimisation des performances de l'application.",
-        ],
-        image: google,
-    },
-    {
-        id: 2,
-        role: "Fullstack Developer",
-        company: "Meta",
-        period: "Jan 2021 - Août 2022",
-        description: [
-            "Création d'une plateforme interne de collaboration pour les équipes.",
-            "Mise en place d'une architecture scalable et optimisée.",
-        ],
-        image: meta,
-    },
-    {
-        id: 3,
-        role: "Frontend Developer",
-        company: "Amazon",
-        period: "Mai 2019 - Déc 2020",
-        description: [
-            "Développement d'une interface utilisateur pour Amazon Web Services.",
-            "Implémentation des tests unitaires et E2E.",
-        ],
-        image: amazon,
-    },
+  {
+    id: 1,
+    role: "Software Engineer",
+    company: "Google",
+    period: "Sep 2022 - Présent",
+    description: [
+      "Développement de nouvelles fonctionnalités pour Google Maps.",
+      "Optimisation des performances de l'application.",
+    ],
+    image: google,
+  },
+  {
+    id: 2,
+    role: "Fullstack Developer",
+    company: "Meta",
+    period: "Jan 2021 - Août 2022",
+    description: [
+      "Création d'une plateforme interne de collaboration pour les équipes.",
+      "Mise en place d'une architecture scalable et optimisée.",
+    ],
+    image: meta,
+  },
+  {
+    id: 3,
+    role: "Frontend Developer",
+    company: "Amazon",
+    period: "Mai 2019 - Déc 2020",
+    description: [
+      "Développement d'une interface utilisateur pour Amazon Web Services.",
+      "Implémentation des tests unitaires et E2E.",
+    ],
+    image: amazon,
+  },
 ];
-
 
 const Experience = () => {
   return (
     <div>
       <Title title="Expériences" />
-      <div className="" >
-        <div className="flex flex-wrap gap-4 justify-center items-center md:w-1/3 mt-4 md:mt-0" >
+      <div className="flex  flex-col-reverse md:flex-row justify-center items-center">
+        <div className="flex flex-wrap gap-4 justify-center items-center md:w-1/3 mt-4 md:mt-0">
           {skills.map((skill) => (
-            <div key={skill.id} className="flex justify-center items-center flex-col ">
-
+            <div
+              key={skill.id}
+              className="flex justify-center items-center flex-col "
+            >
               <div className="w-24 h-24 p-2 rounded-full border-2 border-accent">
-                <img src={skill.image} 
-                  alt={skill.name} 
-                  className="w-full h-full object-cover rounded-full" />
+                <img
+                  src={skill.image}
+                  alt={skill.name}
+                  className="w-full h-full object-cover rounded-full"
+                />
               </div>
               <span className="text-sm mt-2">{skill.name}</span>
-
             </div>
           ))}
         </div>
 
         <div className="md:ml-4 flex flex-col space-y-4">
           {experiences.map((experience) => (
-            <div key={experience.id} className="flex justify-center items-center flex-col ">
-
-              <div className="w-24 h-24 p-2 rounded-full border-2 border-accent">
-                <img src={experience.image} 
-                  alt={experience.company} 
-                  className="w-full h-full object-cover rounded-full" />
-              <ul></ul>
+            <div
+              key={experience.id}
+              className="flex flex-col bg-base-200 p-5 rounded-xl shadow-lg"
+            >
+              <div className="flex items-center">
+                <img
+                  src={experience.image}
+                  alt={experience.company}
+                  className="object-cover h-10 w-10"
+                />
+                <div className="ml-4">
+                  <h1 className="text-xl text-accent font-bold">
+                    {experience.role} , {experience.company}
+                  </h1>
+                  <span className="text-sm">{experience.period}</span>
+                </div>
               </div>
-              <ul></ul>
-              <span className="text-sm mt-2">{experience.name}</span>
 
+              <ul className="list-disc ml-16 mt-2">
+                {experience.description.map((desc, index) => (
+                  <li key={index}>{desc}</li>
+                ))}
+              </ul>
             </div>
           ))}
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
