9066e77dedaa314a443bb0c1f42b9aaff03158f8
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-09 21:46:12 +0100

    first commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,19 @@
+import { Mail } from "luciDe-react";
+
 const Home = () => {  
   return (
     <div className="flex flex-col-reverse md:flex-row items-center justify-center md:my-32 my-10">
-      <div className="d">
+      <div className="flex flex-col">
         <h1 className="text-5xl md:text-6xl font-bold text-center md:text-left mt-4 md:mt-0">Bonjour , <br /> Je suis {" "}
           <span className="text-accent">PerfectoDev</span> 
         </h1>
         <p className="my-4 text-md text-center md:text-left mt-4 text-gray-100">
-          Je suis un Développeur Fulfstack <br /> Passionné par la création d'applications innovantes et performantes.<br /> Contactez moi si vos avez besoin de mes services
+          Je suis un Développeur Fulfstack <br /> Passionné par la création d'applications innovantes et performantes.<br /> Contactez moi si vous avez besoin de mes services
         </p>
 
-        <a href="">
-          
+        <a href="" className="btn btn-accent md:w-fit">
+          <Mail className="w-5 h-5"/>
+          Contactez moi
         </a>
     
       </div>
