2f961efdc401648198977c75a23bbe5aaec74471
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-09 18:03:57 +0100

    first commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,9 @@ const Home = () => {
   return (
     <div className="flex flex-col-reverse md:flex-row items-center justify-center md:my-32 my-10">
       <div className="d">
-c
+        <h1 className="">Bonjour , <br /> je suis 
+          <span className="">PerfectoDev</span> 
+        </h1>
       </div>
 
       <div className="d">x</div>
