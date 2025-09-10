0c6f8ae116d82005655ebb543be72198ff5e71e4
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-10 09:59:11 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Home from "./components/Home";
 import NavBar from "./components/Navbar";
-import NavBar from "./components/Footer";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
