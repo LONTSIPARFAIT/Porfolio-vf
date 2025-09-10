001fb13265b7d090cfc300eff79d0727fdb3d59a
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-10 06:17:28 +0100

    first commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import img from "../assets/img3.jpg";
 
 const Home = () => {  
   return (
-    <div className="flex flex-col-reverse md:flex-row items-center justify-center md:my-32 my-10">
+    <div id="home" className="flex flex-col-reverse md:flex-row items-center justify-center md:my-32 my-10">
 
       <div className="flex flex-col">
         <h1 className="text-5xl md:text-6xl font-bold text-center md:text-left mt-4 md:mt-0">Bonjour , <br /> Je suis {" "}
